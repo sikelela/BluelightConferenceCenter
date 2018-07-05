69043e7d67abee0b36d5627d13e58bd89974afc9
SNkohla <snkohla@singular.co.za>
2018-07-05 11:47:55 +0200

    Add Simple Classes

---
+++ BlueLghtConferenceCenter/BlueLghtConferenceCenter/Controllers/BookingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BlueLghtConferenceCenter.Controllers
+{
+  public class BookingController : System.Web.Mvc.Controller
+  {
+    // GET: Booking
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ BlueLghtConferenceCenter/BlueLghtConferenceCenter/Models/Venue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BlueLghtConferenceCenter.Models
+{
+  public class Venue
+  {
+    public int VenueID { get; set; }
+    public string VenueName { get; set; }
+    public int RoomTypeID { get; set; }
+    public int CapacityStanding { get; set; }
+    public int CapacitySeated { get; set; }
+    public int Seats { get; set; }
+    public DateTime Archive { get; set; }
+  }
+
+  //public class MovieDBContext : DbContext
+  //{
+  //  public DbSet<Venue> Venues { get; set; }
+  //}
+}
